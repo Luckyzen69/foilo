2e881b38b55761fd0f2914333266a8db08cb146f
Lagzen Thakuri <thakurizen2@gmail.com>
2023-07-19 20:33:39 +0545

    sixth commit

--- script.js
+++ script.js
@@ -2,3 +2,14 @@ const header = document.querySelector("header");
 window.addEventListener ("scroll",function(){
     header.classList.toggle ("sticky",window.scrollY > 100);
 });
+function MyCv(){
+    var link = document.createElement('link');
+    //creating a downloading file
+    link.href = 'file:///C:/Users/thaku/Documents/GitHub/foilo/assest%20image/mycv.pdf';
+    link.download = 'LagzenThakuri.png';
+    //simulating a click on the download link
+    document.body.appendChild(link);
+    link.click();
+    // clean up temporary link elements
+    document.body.removeChild(link);
+}
